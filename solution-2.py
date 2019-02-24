5eb8f971080cb9f333c7212e040931d1cd253818
FaceValue687 <dariaz@eircom.net>
2019-02-24 21:51:39 +0000

    Worked solution 2.

--- solution-2.py
+++ solution-2.py
@@ -1,1 +1,8 @@
 #Solution to problem 2
+#This program outputs whether or not today is a day that begins 
+#with the letter T
+import datetime
+if datetime.datetime.today().weekday() == 'T':
+   print('Yes - today begins with a T.')
+else:
+   print('No, today does not begin with a T.')
