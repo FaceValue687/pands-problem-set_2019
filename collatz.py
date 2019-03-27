f2b53912074861db86aebd330cc1c5018d04269e
FaceValue687 <dariaz@eircom.net>
2019-03-27 20:42:43 +0000

    collatz.py

--- collatz.py
+++ collatz.py
@@ -5,11 +5,27 @@
 #the current value is one.
 #Daria J. Ostrowska
 
-
-num = int(input("Please enter a positive integer: "))
-while num!=1:
-    if num%2 == 0:
-        num = num/2
+#I set up a "while True" loop to repeat a section of code that takes the user input in case of wrong input
+#User input must be>1 as the Collatz conjecture defines a sequence of positive integers that starts with any 
+#number>1.
+while True:
+   try:
+    	number = int(input("Please enter a positive integer bigger than 1: "))
+#I use the "assert" condition to be able to raise an exception if the above condition is not met
+    	assert(number>1)
+#and if the condition is true I set the program to continue from there 
+    	break
+#If the condition is not true, exception is raised
+   except:
+       print("Number entered must be bigger than")
+ 
+while number!=1:
+    if number%2 == 0:
+        number = number/2
     else:
-        num = (num*3)+1
-    print((num)
+        number = (number*3)+1
+    print(number)
+
+
+#Based on: https://www.cis.upenn.edu/~matuszek/cit590-2016/Lectures/15%20Functional%20Python.pdf
+#      and:https://www.quora.com/What-does-while-true-do-in-python  
