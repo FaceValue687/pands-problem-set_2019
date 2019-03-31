af577c3a2473f0d7668dbc4a030aed2f24d1e2b9
FaceValue687 <dariaz@eircom.net>
2019-03-31 18:06:05 +0100

    Updates

--- second-moby-dick.py
+++ second-moby-dick.py
@@ -2,11 +2,19 @@
 #on the command line.
 #Daria J. Ostrowska
 
-#
-text_file=open("Tom_Jones_a_chapter.txt","r")
-with open("Tom_Jones_a_chapter.txt", "r+") as work_file:
+##A program that reads in a text file and outputs every second line, taking the file name from an argument
+#on the command line.
+#Daria J. Ostrowska
+
+#I invite user input ("Tom_Jones_a_chapter.txt")
+user_input = (input("Enter the name of the file : "))
+#The program will open and read the user's input
+with open(user_input, "r+") as work_file:
+#and count lines starting from line #1
     for count, line in enumerate (work_file, start=1):
+#if line number is not divisible by 2
         if count%2==0:
+#it will print that line
             print(line)
 
 
